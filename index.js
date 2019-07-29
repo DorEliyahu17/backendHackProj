5bd2b8d8dd165f526a434a86dbf437a26e15e014
Arad Gonen <gonena.arad@gmail.com>
2019-07-29 22:36:39 +0300

    fix merge

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 const cron = require("node-cron");
 const sendPic = require("./sendPic.js");
-<<<<<<< Updated upstream
-const actionRun = require("./actionRun.js");
 
 cron.schedule("0 * * * * *", () => {
   //sendPic();
-=======
-cron.schedule("0 * * * * *", () => {
-  console.log(`this message logs every minute`);
-  sendPic();
-});
-cron.schedule("15 * * * * *", () => {
-  console.log(`this message logs every minute`);
-  sendPic();
-});
-cron.schedule("30 * * * * *", () => {
-  console.log(`this message logs every minute`);
-  sendPic();
-});
-cron.schedule("45 * * * * *", () => {
-  console.log(`this message logs every minute`);
-  sendPic();
->>>>>>> Stashed changes
-});
+  cron.schedule("0 * * * * *", () => {
+    sendPic();
+  });
+  cron.schedule("15 * * * * *", () => {
+    console.log(`this message logs every minute`);
+    sendPic();
+  });
+  cron.schedule("30 * * * * *", () => {
+    console.log(`this message logs every minute`);
+    sendPic();
+  });
+  cron.schedule("45 * * * * *", () => {
+    console.log(`this message logs every minute`);
+    sendPic();
+  });
 
-cron.schedule("30 * * * * *", () => {
   //sendPic();
 });
 
